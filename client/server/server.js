7bf38f37cc5c575bd2fc4a8c715d4a41df615221
Jonathan Pohahau <j.pohahau@outlook.com>
2023-03-07 10:28:24 -0800

    initial commit

---
+++ client/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+});
+
+
+// first use a for loop to iterate through the array  wand if there is a matching pair to return that number correlated with that matching index
+//  come pair the  value of sum to the  value of the matching pair added together with a conditional statement if they don't match return false if they do return true
